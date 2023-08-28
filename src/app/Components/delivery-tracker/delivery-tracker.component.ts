913d0e09b915d2345146a248e064092d6fbd0789
Shiva Shanker Parampovu <ShivaShanker.Parampovu@Kantar.com>
2023-08-28 16:22:24 +0530

    delivery tracker app 01

---
+++ src/app/Components/delivery-tracker/delivery-tracker.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-tracker',
+  templateUrl: './delivery-tracker.component.html',
+  styleUrls: ['./delivery-tracker.component.css']
+})
+export class DeliveryTrackerComponent implements OnInit{
+orderId:Number=123
+deliveryAgentName:string='john D'
+customerName: String = 'Vijay Kumar'
+deliveryAddress: Array<String> = ['225 Baker Street']
+orderPrice: Number = 900
+orderVerified: String = 'Order not Verified!'
+orderPaymentStatus:String = 'Paid'
+productImg: String = 'https://static.toiimg.com//photo//msid87930581//87930581.jpg'
+
+constructor(){
+
+}
+
+ngOnInit(): void {
+    
+}
+verifyOrder(){
+this.orderVerified='Order is Verified'
+}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeliveryTrackerComponent } from './Components/delivery-tracker/delivery-tracker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeliveryTrackerComponent
   ],
   imports: [
     BrowserModule,
